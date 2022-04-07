2bbab1c5c61c1de60453397d38e8c11907d121cf
ozi <otabekzadaev@gmail.com>
2022-04-07 15:37:24 +0700

    DT

--- Telegram.Bot/Configuration.cs
+++ Telegram.Bot/Configuration.cs
@@ -3,6 +3,6 @@ namespace Telegram.Bot.VoiceMemBot
 {
     public static class Configuration
     {
-        public readonly static string BotToken = "5287756049:AAFY4Hly7IbWSzzZMGMHOqET5P0GSIDGLrI";
+        public readonly static string BotToken = "TOKEN";
     }
 }
